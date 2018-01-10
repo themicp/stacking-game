f773c474c0170e40ced0cd0573e46a6012360c6e
Themis Papameletiou <themicp@gmail.com>
2018-01-10 16:13:00 +0200

    [INO]: Fix stopping conditions

--- game.ino
+++ game.ino
@@ -120,13 +120,13 @@ void stickBar() {
         }
     }
 
-    if (barLength == 0) {
+    if (barLength <= 0) {
         Serial.print("You lost! Reboot to start again.");
         gameComplete = true;
         return;
     }
 
-    if (barRow == 0) {
+    if (barRow <= 0) {
         Serial.print("Congratulations, you won! Reboot to start again.");
         gameComplete = true;
         return;
